c6f81bb6065595bf7a0b9af49b13ca6284870840
Sebb i9x <sebastian.neubert@posteo.de>
2024-08-19 19:59:30 +0200

    update project with flowcv data

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     env: {
-      SITE_UNDER_TEST: 'https://docs.cypress.io/'
+      SITE_UNDER_TEST: 'https://flowcv.me/sebastian-neubert-2k'
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/career-site.cy.ts
+++ cypress/e2e/career-site.cy.ts
@@ -6,11 +6,33 @@ describe(`check ${siteUnderTest}`, () => {
     cy.visit(siteUnderTest)
   })
 
-  it('should have a career link', () => {
-    cy.findByRole('link', { name: /careers?|jobs?/i })
+  it('Should have basic components', () => {
+    cy.findByRole('heading', { name: /sebastian neubert/i })
       .should('have.length', 1)
-      .as('careerLink')
 
-    cy.get('@careerLink').click()
+    cy.findByRole('heading', { name: /senior quality engineer/i })
+      .should('have.length', 1)
+
+    cy.get('a[href]').should('have.length.at.least', 5).as('siteLinks')
+
+    cy.get('@siteLinks').each((link)=>{
+      expect(link).to.have.attr('href')
+      expect(link.attr('href')).to.match(/https:|mailto:/i)
+    })
+  })
+
+  it('Should see button to career', () => {
+    cy.findByRole('link', {name: /career project/i})
+      .should('be.visible')
+      .as('careerButton')
+
+    cy.get('@careerButton').should('have.attr','href', 'https://github.com/sebastianneubert/career')
+    cy.get('@careerButton').should('have.attr','target', '_blank')
+    cy.get('@careerButton').invoke('removeAttr', 'target').click()
+
+    cy.origin('https://github.com', () => {
+      cy.get('h1').should('contain', 'career summary', {matchCase:false})
+      cy.url().should('be.equal', 'https://github.com/sebastianneubert/career')
+    })
   })
 })
