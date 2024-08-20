0693c180b805cef8fe474243534642b309c46f2f
Sebb i9x <sebastian.neubert@posteo.de>
2024-08-20 19:23:03 +0200

    add html report

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,17 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from 'cypress'
+import getSslCert from 'get-ssl-certificate'
+import reportingPlugin from 'cypress-mochawesome-reporter/plugin'
 
-module.exports = defineConfig({
+export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'SPVOS Frontend Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    saveJson: true,
+  },
   e2e: {
     env: {
       SITE_UNDER_TEST: 'https://flowcv.me/sebastian-neubert-2k'
@@ -8,10 +19,20 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       on('task', {
         getCertificate: (host) => {
-          const sslCertificate = require('get-ssl-certificate')
-          return sslCertificate.get(host)
+          return getSslCert.get(host)
         }
       })
+      reportingPlugin(on)
+      return config
     },
+  },
+  retries: {
+    experimentalStrategy: 'detect-flake-and-pass-on-threshold',
+    experimentalOptions: {
+      maxRetries: 2,
+      passesRequired: 1,
+    },
+    runMode: true,
+    openMode: true,
   },
 });
